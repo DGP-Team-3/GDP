29a5944dd00a264cf86c9fd44a065b4f3d35c455
kris <Kristopher.Kath@gmail.com>
2022-07-05 17:51:11 -0700

    Added Skeleton Classes and fixed Art assets quality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject); //Make persistant
+        }
+    }
+
+
+}
